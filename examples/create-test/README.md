8398cfd7049eddd2e00684a8a81e2773ed1af6e8
Neil Mansilla <neil@mansilla.com>
2015-08-18 14:50:57 -0700

    Sample app for creating tests via API

---
+++ examples/create-test/README.md
@@ -0,0 +1,34 @@
+Runscope Tests in JSON
+============
+A collection of tests in JSON that you can instantly
+import into your Runscope account via the API. Also
+included is a sample app that will make the POST
+API call.
+
+This serves as an example of how easy it is to create
+tests programmatically with the Runscope API.
+
+Requirements
+------------
+- Python
+- Python Requests library
+- Runscope API key (access token)
+
+Configuration
+------------
+Copy ```config.json.sample``` to ```config.json``` and then 
+make the necessary modifications (add your access token
+from Runscope).
+
+Obtaining an Access Token
+------------
+To create an access token, login to your Runscope account and navigate to [https://www.runscope.com/applications](https://www.runscope.com/applications). Here, click ```Create Application``` -- for name, type in *Test Backup App*. For website and callback URLs, you can just type in ```https://www.runscope.com``` as placeholders (these URLs are never used -- we're just creating an application for the personal access token). After you've created the application, copy the personal access token (i.e. ```5ffffffe-ab99-43ff-7777-3333deee99f9```) and use it in ```config.json```.
+
+Running the App
+------------
+$ python create_test.py tests/deck_of_cards_api_test.json
+$ python create_test.py tests/minimal_test.json
+
+ChangeLog
+------------
+2015-08-18 - Initial release.
---
+++ examples/create-test/create_test.py
@@ -0,0 +1,85 @@
+import json
+import requests
+import sys
+import inspect
+
+# Global dictionary
+g = {}
+
+# Retrieve name from account resource
+def get_account_info():
+    j = _api_get_request( '/account', 200 )
+    g['name'] = j['name']
+    print '\nHello %s.\n' % g['name']
+
+# Retrieves list of buckets for the authed account
+def get_bucket_list():
+    return _api_get_request( '/buckets', 200 )
+
+# Bucket selection (user input from terminal)
+def select_bucket():
+    get_account_info()
+    buckets = get_bucket_list()
+
+    for x in range(0,len(buckets)):
+        print '%s - %s (%s)' % ( x, buckets[x]['name'], buckets[x]['key'] )
+
+    selection = -1
+    while (selection < 0 or selection > len(buckets)-1):
+        selection = input ('\nSelect a bucket for this imported test: ')
+
+    g['bucket_key'] = buckets[selection]['key']
+
+# Read and validate test from JSON file
+def read_test():
+    try:
+        f = open(sys.argv[1], 'r')
+	t = f.read()
+        j = json.loads(t)
+	j['name'] = j['name']
+    except IOError as e:
+        sys.exit("\nI/O error({0}): {1}\n".format(e.errno, e.strerror))
+    except ValueError:
+        sys.exit("\nInvalid JSON in file %s\n" % sys.argv[1])
+    except KeyError:
+        sys.exit("\nInvalid test definition JSON in file %s: Test name is required.\n" % sys.argv[1])
+
+    g['new_test'] = json.dumps(j)
+
+# Create test with API
+def create_test():
+    data = _api_post( '/buckets/%s/tests' % g['bucket_key'], g['new_test'], 201)
+    print "\nNew test created.\n"
+
+# Execute HTTP GET request
+def _api_get_request( path, status ):
+    r = requests.get('%s/%s' % (g['base_url'],path), headers=g['headers'])
+    if (r.status_code != status):
+        _api_error_exit( r.status_code )
+    return (json.loads(r.text))['data']
+
+# Execute HTTP POST request
+def _api_post( path, data, status ):
+    r = requests.post('%s/%s' % (g['base_url'],path), data=data, headers=g['headers'])
+    if (r.status_code != status):
+        _api_error_exit( r.status_code )
+    return (json.loads(r.text))['data']
+
+# Exits on API error, displaying status code and function
+# name where error occurred.
+def _api_error_exit( status_code ):
+    sys.exit('API error - HTTP status code %s in %s' % (status_code,inspect.stack()[1][3]))
+
+def main():
+    with open('config.json') as config_file:
+	config = json.load(config_file)
+
+    g['headers'] = {'Authorization':'Bearer %s' % config["runscope"]["access_token"],'Content-type':'application/json'}
+    g['base_url'] = 'https://api-runscope-com-9mgt30zia7pm.runscope.net'
+
+    read_test()
+    select_bucket()
+    create_test()
+    sys.exit()
+
+if __name__ == "__main__": main()
